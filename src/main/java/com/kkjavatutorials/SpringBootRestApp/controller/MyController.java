53e4fe30b3357b1c45611343032a5ce8c76303b6
shivanand4761 <110733118+shivanand4761@users.noreply.github.com>
2022-08-26 23:15:54 +0530

    Update MyController.java

--- src/main/java/com/kkjavatutorials/SpringBootRestApp/controller/MyController.java
+++ src/main/java/com/kkjavatutorials/SpringBootRestApp/controller/MyController.java
@@ -8,7 +8,7 @@ public class MyController {
 	
 	@GetMapping(path = "/welcome")
 	public String welcome() {
-		return "Welcome to KK JavaTutorials..";
+		return "Spring boot app. This created using the gradle";
 	}
 
 }
